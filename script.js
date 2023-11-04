ef48e6d12906f7a63950fc8dfef8d428efed6051
baibhab_ranjan <134831003+BaibhabRanjan@users.noreply.github.com>
2023-11-04 18:18:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: [" an Engineer", "a Developer", "a Blogger", "a Designer", "a Freelancer"],
+        strings: [" an Engineer", "a Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["an Engineer", "a Developer", "a Blogger", "a Designer", "a Freelancer"],
+       strings: [" an Engineer", "a Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
